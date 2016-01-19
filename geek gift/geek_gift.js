9247fb60bf7d47627124f1e881a0887f101a2de7
kingvid-chan <kingvid.chan@gmail.com>
2016-01-19 22:31:27 +0800

    2

--- geek gift/geek_gift.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-	touch.on(".seaHouse-list-inner","touchstart hold tap",function(){
-		$(this).find(".qw_border").addClass("active");
-		$(this).siblings().find(".qw_border").removeClass("active");
-	})
-})
